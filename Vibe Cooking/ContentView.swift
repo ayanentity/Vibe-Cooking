bcf43d3429bf816bf9cd83e39ed35aa14995a95e
ayanentity <ayanentity154@gmail.com>
2026-06-10 20:45:10 +0900

    initial commit

---
+++ Vibe Cooking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vibe Cooking
+//
+//  Created by 仲里絢音 on 2026/06/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vibe Cooking/Vibe_CookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vibe_CookingApp.swift
+//  Vibe Cooking
+//
+//  Created by 仲里絢音 on 2026/06/10.
+//
+
+import SwiftUI
+
+@main
+struct Vibe_CookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
